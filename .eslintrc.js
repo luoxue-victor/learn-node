dc049d1781c32e1d460d350e493103b170c7df1d
luoxue <luoxue.123@bytedance.com>
2020-04-22 16:02:08 +0800

    ci(all): 构建项目

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  }
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 1.0.0 (2020-04-22)
+
+范围|描述|commitId
+--|--|--
+ - | Initial commit | [61d56a5](https://github.com/luoxue-victor/learn-node/commit/61d56a5)
+
